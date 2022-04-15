aca9b620cb5f57b1903e44e0cefc769a4bce1400
shivcx <shivramas3556@gmail.com>
2022-04-15 17:47:15 +0530

    search bar

--- src/Cart/index.js
+++ src/Cart/index.js
@@ -1,31 +1,45 @@
-import {MdOutlineMenu, MdSearch} from 'react-icons/md';
-import {BsCamera} from 'react-icons/bs';
-import './style.css';
-import ProductList from '../ProductList';
+import { MdOutlineMenu, MdSearch } from "react-icons/md";
+import { BsCamera } from "react-icons/bs";
+import "./style.css";
+import ProductList from "../ProductList";
 
 const Cart = () => {
-    return (
-        <>
-            <div className="navbar">
-                
-                    < MdOutlineMenu style={{width: "2em",
-    height: "auto"}}/>
-                <h4>LOGO.</h4>
-            </div>
-            <div className='max-width'>
-                < MdSearch style={{position: "absolute", paddingTop: "5px",
-    paddingLeft: "5px"}}/>
-                <BsCamera style={{position: "absolute", right: "20px", paddingTop: "4px"}} />
-                <span>
-                    <input name='search' type="text" style={{width: "100%"}}/>
+  return (
+    <>
+      <div className="navbar">
+        <MdOutlineMenu style={{ width: "2em", height: "auto" }} />
+        <h4>LOGO.</h4>
+      </div>
+      <div className="max-width">
+        <MdSearch style={{ position: "absolute", padding: "11px" }} />
+        <span>
+          <input
+            name="search"
+            type="text"
+            style={{
+              width: "90%",
+              backgroundColor: "#ededed",
+              border: "none",
+              padding: ".8em",
+              paddingLeft: "2.5em",
+            }}
+            placeholder="Search"
+          />
+        </span>
+        <BsCamera
+          style={{
+            position: "absolute",
+            right: "20px",
+            borderLeft: "solid 1px gray",
+            margin: "11px",
+            paddingLeft: "11px",
+          }}
+        />
 
-                </span>
+        <ProductList />
+      </div>
+    </>
+  );
+};
 
-                <ProductList/>
-            </div>
-        </>
-    )
-
-}
-
-export default Cart
+export default Cart;
